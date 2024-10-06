1ab751e26a8710b711d9bafbf8852c9aba6f3810
t0nn1x <antoonhrobust@gmail.com>
2024-10-06 14:14:26 +0200

    first commit

---
+++ lab-1/lab-1.py
@@ -0,0 +1,11 @@
+import numpy as np
+from sklearn import preprocessing
+
+input_data = np.array([[5.1, -2.9, 3.3],
+                       [-1.2, 7.8, -6.1],
+                       [3.9, 0.4, 2.1],
+                       [7.3, -9.9, -4.5]])
+
+# Бінаризація даних
+data_binarized = preprocessing.Binarizer(threshold=2.1).transform(input_data)
+print("\n Binarized data:\n", data_binarized)
