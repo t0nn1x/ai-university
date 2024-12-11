c02c9d100bbfc5b3f39fb2e381c550301f08d5bd
t0nn1x <antoonhrobust@gmail.com>
2024-12-11 21:24:10 +0100

    Lab 5 task 1

--- lab-5/random_forests.py
+++ lab-5/random_forests.py
@@ -1,24 +1,22 @@
-import argparse 
-
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split  # Виправлений імпорт
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
-from sklearn.metrics import classification_report
-
 from utilities import visualize_classifier
 
-# Argument parser 
+# Argument parser
 def build_arg_parser():
     parser = argparse.ArgumentParser(description='Classify data using \
             Ensemble Learning techniques')
-    parser.add_argument('--classifier-type', dest='classifier_type', 
-            required=True, choices=['rf', 'erf'], help="Type of classifier \
+    parser.add_argument('--classifier-type', dest='classifier_type',
+                        required=True, choices=['rf', 'erf'], help="Type of classifier \
                     to use; can be either 'rf' or 'erf'")
     return parser
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     # Parse the input arguments
     args = build_arg_parser().parse_args()
     classifier_type = args.classifier_type
@@ -29,23 +27,23 @@ if __name__=='__main__':
     X, y = data[:, :-1], data[:, -1]
 
     # Separate input data into three classes based on labels
-    class_0 = np.array(X[y==0])
-    class_1 = np.array(X[y==1])
-    class_2 = np.array(X[y==2])
+    class_0 = np.array(X[y == 0])
+    class_1 = np.array(X[y == 1])
+    class_2 = np.array(X[y == 2])
 
     # Visualize input data
     plt.figure()
-    plt.scatter(class_0[:, 0], class_0[:, 1], s=75, facecolors='white', 
-                    edgecolors='black', linewidth=1, marker='s')
-    plt.scatter(class_1[:, 0], class_1[:, 1], s=75, facecolors='white', 
-                    edgecolors='black', linewidth=1, marker='o')
-    plt.scatter(class_2[:, 0], class_2[:, 1], s=75, facecolors='white', 
-                    edgecolors='black', linewidth=1, marker='^')
+    plt.scatter(class_0[:, 0], class_0[:, 1], s=75, facecolors='white',
+                edgecolors='black', linewidth=1, marker='s')
+    plt.scatter(class_1[:, 0], class_1[:, 1], s=75, facecolors='white',
+                edgecolors='black', linewidth=1, marker='o')
+    plt.scatter(class_2[:, 0], class_2[:, 1], s=75, facecolors='white',
+                edgecolors='black', linewidth=1, marker='^')
     plt.title('Input data')
 
-    # Split data into training and testing datasets 
-    X_train, X_test, y_train, y_test = train_test_split.train_test_split(
-            X, y, test_size=0.25, random_state=5)
+    # Split data into training and testing datasets
+    X_train, X_test, y_train, y_test = train_test_split(  # Виправлений виклик
+        X, y, test_size=0.25, random_state=5)
 
     # Ensemble Learning classifier
     params = {'n_estimators': 100, 'max_depth': 4, 'random_state': 0}
@@ -64,7 +62,8 @@ if __name__=='__main__':
     class_names = ['Class-0', 'Class-1', 'Class-2']
     print("\n" + "#"*40)
     print("\nClassifier performance on training dataset\n")
-    print(classification_report(y_train, classifier.predict(X_train), target_names=class_names))
+    print(classification_report(y_train, classifier.predict(
+        X_train), target_names=class_names))
     print("#"*40 + "\n")
 
     print("#"*40)
@@ -73,17 +72,18 @@ if __name__=='__main__':
     print("#"*40 + "\n")
 
     # Compute confidence
-    test_datapoints = np.array([[5, 5], [3, 6], [6, 4], [7, 2], [4, 4], [5, 2]])
+    test_datapoints = np.array(
+        [[5, 5], [3, 6], [6, 4], [7, 2], [4, 4], [5, 2]])
 
     print("\nConfidence measure:")
     for datapoint in test_datapoints:
         probabilities = classifier.predict_proba([datapoint])[0]
         predicted_class = 'Class-' + str(np.argmax(probabilities))
         print('\nDatapoint:', datapoint)
-        print('Predicted class:', predicted_class) 
+        print('Predicted class:', predicted_class)
 
     # Visualize the datapoints
-    visualize_classifier(classifier, test_datapoints, [0]*len(test_datapoints), 
-            'Test datapoints')
+    visualize_classifier(classifier, test_datapoints, [0]*len(test_datapoints),
+                         'Test datapoints')
 
     plt.show()
